26071b436622650c6f9a499545784f7e75871e56
J Resilient <j.resilient.1@gmail.com>
2019-10-10 19:24:39 -0400

    Add #winning_node?

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -20,6 +20,12 @@ class TicTacToeNode
   end
 
   def winning_node?(evaluator)
+    return true if board.over? && board.winner == evaluator
+    return false if board.over? && (board.winner != evaluator || board.winner.nil?)
+
+    next_states = self.children
+    return next_states.any? { |child| child.winning_node?(evaluator) } if next_mover_mark == evaluator
+    next_states.all? { |child| child.winning_node?(evaluator) } if next_mover_mark != evaluator
   end
 
   # This method generates an array of all moves that can be made after
