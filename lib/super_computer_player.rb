0fc5751593c84784a2da26949fd756bb786f9441
J Resilient <j.resilient.1@gmail.com>
2019-10-10 20:47:50 -0400

    Add #move

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -1,13 +1,22 @@
 require_relative 'tic_tac_toe_node'
+require 'byebug'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+
+    node.children.each { |child| return child.prev_move_pos if child.winning_node?(mark) }
+    node.children.each do |child|
+      return child.prev_move_pos unless child.losing_node?(mark) 
+    end
+    
+    raise "Something's gone wrong: I should be able to force a tie.\n"
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Play the brilliant computer!"
-  hp = HumanPlayer.new("Jeff")
+  hp = HumanPlayer.new("Alice")
   cp = SuperComputerPlayer.new
 
   TicTacToe.new(hp, cp).run
