227e1999c1cd69e00f6a79c1af9377cc70a54bce
J Resilient <j.resilient.1@gmail.com>
2019-10-11 11:49:02 -0400

    Refactor based on app_academy's solution

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -16,17 +16,24 @@ class TicTacToeNode
     end
 
     next_states = self.children
-    return next_states.all? { |child| child.losing_node?(evaluator) } if next_mover_mark == evaluator
-    next_states.any? { |child| child.losing_node?(evaluator) } if next_mover_mark != evaluator
+    if next_mover_mark == evaluator
+      next_states.all? { |child| child.losing_node?(evaluator) } 
+    else
+      next_states.any? { |child| child.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
-    return true if board.over? && board.winner == evaluator
-    return false if board.over? && (board.winner != evaluator || board.winner.nil?)
+    if board.over?
+      return board.winner == evaluator
+    end
 
     next_states = self.children
-    return next_states.any? { |child| child.winning_node?(evaluator) } if next_mover_mark == evaluator
-    next_states.all? { |child| child.winning_node?(evaluator) } if next_mover_mark != evaluator
+    if next_mover_mark == evaluator
+      next_states.any? { |child| child.winning_node?(evaluator) } 
+    else
+      next_states.all? { |child| child.winning_node?(evaluator) }
+    end
   end
 
   # This method generates an array of all moves that can be made after
@@ -35,15 +42,18 @@ class TicTacToeNode
     children = []
     (0...3).each do |row_idx|
       (0...3).each do |col_idx|
-        if board.empty?([row_idx, col_idx])
+        pos = [row_idx,col_idx]
+
+        if board.empty?(pos)
           new_board = board.dup
-          pos = [row_idx,col_idx]
           new_board[pos] = next_mover_mark
           new_mover_mark = next_mover_mark == :x ? :o : :x
           children << TicTacToeNode.new(new_board, new_mover_mark, pos)
         end
+
       end
     end
+    
     children
   end
 end
